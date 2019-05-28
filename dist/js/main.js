ffa8c11b7ce8dd7582a5ca34980f353780c6c5a9
EditaA <editasefullaj@gmail.com>
2019-05-29 00:29:41 +0200

    finished styling home page and navigation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,10 @@
-console.log("ok")
+const burger = document.querySelector(".burger");
+const body = document.querySelector("body")
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("is-active");
+    body.classList.toggle("menu-shown")
+
+})
+
+
