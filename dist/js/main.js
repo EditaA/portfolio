6b14d39129b55ee1fc6075c49e0977b8b3f8c359
EditaA <editasefullaj@gmail.com>
2019-07-19 23:39:35 +0200

    completed front end

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,8 @@
 const burger = document.querySelector(".burger");
 const body = document.querySelector("body")
 
-burger.addEventListener("click", () => {
+burger.addEventListener("click", function () {
     burger.classList.toggle("is-active");
     body.classList.toggle("menu-shown")
-
 })
-
 
