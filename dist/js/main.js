41389526a64edae35f85cbe092c02d408f45d4f4
editaS <edita@cinepass.de>
2020-04-20 19:44:01 +0200

    some bug fixes

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,9 @@ const burger = document.querySelector(".burger");
 const body = document.querySelector("body")
 
 burger.addEventListener("click", function () {
-    burger.classList.toggle("is-active");
+    console.log('burger clicked')
+    burger.classList.toggle("is-active")
     body.classList.toggle("menu-shown")
 })
-
+// Contact Form
+// const form = document.querySelector('.form');
